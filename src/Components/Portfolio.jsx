61bb81186f2021341ed5ee6e58bae272642e738a
ahmedshefo <a.shefoo3333@gmail.com>
2023-12-16 19:14:15 +0200

    add all changes

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/Computer.jpg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "Labtop computer";
 
 /**
  * Project list
@@ -31,25 +31,25 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "My LinkedIn",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "Here is my LinkedIn account check it.",
+    url: "https://www.linkedin.com/in/ahmed-shefoo/",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Ahmed shefoo",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
+      "It's Good To Be Back.",
     url: "https://github.com/microsoft/web-dev-for-beginners",
   },
   {
-    title: "My Resume Site",
+    title: "I Find It's Best To Wait...For The Perfect code",
     description:
       "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
     url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "I'm Sorry. You're Safe Now. It's Okay",
     description:
       "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
     url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
